0704b2e536f4811a8f130433b99060aff17ddbaa
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2023-11-13 17:03:49 +0100

    Test commit

--- LaChevauchéeFantastique/src/lachevauchéefantastique/LaChevauchéeFantastique.java
+++ LaChevauchéeFantastique/src/lachevauchéefantastique/LaChevauchéeFantastique.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+LA CHEVAUCHEE FANTASTIQUE, 13/11/2023, SARINENA-DUMAS-BRUNET MANQUAT
  */
 package lachevauchéefantastique;
 
